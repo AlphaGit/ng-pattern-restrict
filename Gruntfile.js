1e242b08370db496edd42eaa8bf8f3901e0ba37b
Alpha <alphagma@gmail.com>
2013-11-10 15:55:19 -0600

    Create Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Runs all unit tests with Karma
+        karma: {
+            options: {
+                configFile: 'karma.e2e.conf.js'
+            },
+            continuous: {
+                singleRun: true,
+                browsers: ['PhantomJS']
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-karma');
+
+    grunt.registerTask('test', [
+        'karma'
+    ]);
+
+    grunt.registerTask('default', [
+        'karma'
+    ]);
+};
